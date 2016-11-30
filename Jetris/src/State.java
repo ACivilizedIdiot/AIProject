a89b64b9802c3cb745966ae417c11cdb59e981ce
nicolas-epstein <nicolas.epstein856@topper.wku.edu>
2016-11-29 20:45:09 -0600

    Update State.java

--- Jetris/src/State.java
+++ Jetris/src/State.java
@@ -4,44 +4,68 @@
 //as you wish
 public class State {
 	
-	protected boolean[][] blocks = new boolean[20][10];
-	protected boolean[][] nextBlock = new boolean[4][4];
+	int[][] grid = new int[Tetris.ROWS][Tetris.COLUMNS];
+	int score = 0;
 	
-	public State(boolean[][] newBlocks, boolean[][] newNextBlock){
-		blocks = deepClone(newBlocks);
-		nextBlock = deepClone(newNextBlock);
+	
+	//values to assess utility of state
+	int[] columnHeights = new int[10];	//heighest occupied cell in each column 
+	int maxHeight = 0;	//heighest occupied cell in ANY column 
+	int emptyCells = 0;
+		
+	public State(int[][] grid, int score, int empty){
+		this.grid = grid;
+		this.score = score;
+		this.emptyCells = empty;
+		
 	}
 	
-	private boolean[][] deepClone(boolean[][] doubleArray){
-		boolean[][] newArray = new boolean[doubleArray.length][doubleArray[0].length];
-		for(int i = 0; i < doubleArray.length; i++){
-			newArray[i] = doubleArray[i].clone();
+	public State(int[] columnHeights, int maxHeight, int emptyCells, int[][] grid, int score){
+		this.columnHeights = columnHeights;
+		this.maxHeight = maxHeight;
+		this.emptyCells = emptyCells;
+		this.grid = grid;
+		this.score = score;
+		
+	}
+	
+	private int[][] deepClone(int[][] grid2){
+		int[][] newArray = new int[grid2.length][grid2[0].length];
+		for(int i = 0; i < grid2.length; i++){
+			newArray[i] = grid2[i].clone();
 		}
 		return newArray;
 	}
-	
+		
 	public boolean equals(State otherState){
-		boolean[][] otherBlocks = otherState.blocks;
-		for(int i = 0; i < blocks.length; i++){
-			for(int j = 0; j < blocks[0].length; j++){
-				if(blocks[i][j] != otherBlocks[i][j]){
+		int[][] otherGrid = otherState.grid;
+		for(int i = 0; i < grid.length; i++){
+			for(int j = 0; j < grid[0].length; j++){
+				if(grid[i][j] != otherGrid[i][j]){
 					return false;
+					
 				}
 			}
-		}
-		boolean[][] otherNextBlock = otherState.nextBlock;
-		for(int i =0; i < nextBlock.length; i++){
-			for(int j = 0; j < nextBlock[0].length; j++){
-				if(nextBlock[i][j] != otherNextBlock[i][j]);
-			}
-		}
-		
+		}		
 		return true;
 	}
 	
-	public State clone(){
-		return new State(deepClone(blocks), deepClone(nextBlock));
+	public int[][] getGrid(){
+		return grid;
 	}
+	
+	public State clone(){
+		return new State(deepClone(grid), score, emptyCells);
+	}
+	
+	public void printState(){
+		//printing out max height of state before next piece enters game
+//		System.out.println("Max Height: " + maxHeight);
+//				System.out.println("Column Height Data: " + Arrays.toString(columnHeights));
+//				System.out.println("Empty Cells: " + emptyCells);
+				System.out.println("Score :" + score);
+	}
+	
 	
 
 }
